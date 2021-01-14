516d42a021741021e0d4429a796e0de7b24e601f
Armando Chindoy <armandojchindoy@gmail.com>
2021-01-14 17:08:29 -0500

    Configs for the dev-enviroment

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Esta es un prueba2</h1>
+        </div>
+    );
+}
+
+export default HelloWorld;
